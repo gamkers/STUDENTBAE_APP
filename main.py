0f597e4bcd3d3bdb2b44942b4a85716eb029c508
AKASH M <87299311+gamkers@users.noreply.github.com>
2023-05-02 18:33:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -190,8 +190,7 @@ def check_password():
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         if (
-            st.session_state["username"] in st.secrets["user"]
-            and st.session_state["password"] == st.secrets["pass"][st.session_state["username"]]
+            st.session_state["username"] in st.secrets["user"] and st.session_state["password"] == st.secrets["pass"][st.session_state["username"]]
         ):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store username + password
